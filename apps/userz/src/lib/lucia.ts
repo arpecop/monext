7c6baea7643e5410d353302bab0b66949a1d1b88
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-10 22:06:41 +0300

    xxx

--- apps/userz/src/lib/lucia.ts
+++ apps/userz/src/lib/lucia.ts
@@ -1,18 +1,18 @@
-import { postgres as postgresAdapter } from "@lucia-auth/adapter-postgresql";
-import { lucia } from "lucia";
-import { astro } from "lucia/middleware";
-// import postgres from "postgres";
-const pgurl = process.env.PGURL as string;
-// const sql = postgres(pgurl);
+// import { postgres as postgresAdapter } from "@lucia-auth/adapter-postgresql";
+// import { lucia } from "lucia";
+// import { astro } from "lucia/middleware";
+// // import postgres from "postgres";
+// const pgurl = process.env.PGURL as string;
+// // const sql = postgres(pgurl);
 
-export const auth = lucia({
-  adapter: postgresAdapter({}, {
-    user: "auth_user",
-    key: "user_key",
-    session: "user_session",
-  }),
-  middleware: astro(),
-  env: "DEV",
-});
+// export const auth = lucia({
+//   adapter: postgresAdapter({}, {
+//     user: "auth_user",
+//     key: "user_key",
+//     session: "user_session",
+//   }),
+//   middleware: astro(),
+//   env: "DEV",
+// });
 
-export type Auth = typeof auth;
+// export type Auth = typeof auth;
