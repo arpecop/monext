b66c3048f4c19d2d5109320dff13d6d15044b106
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-10 22:05:40 +0300

    dddd

--- apps/userz/src/lib/lucia.ts
+++ apps/userz/src/lib/lucia.ts
@@ -1,12 +1,12 @@
-import { lucia } from "lucia";
 import { postgres as postgresAdapter } from "@lucia-auth/adapter-postgresql";
+import { lucia } from "lucia";
 import { astro } from "lucia/middleware";
-import postgres from "postgres";
+// import postgres from "postgres";
 const pgurl = process.env.PGURL as string;
-const sql = postgres(pgurl);
+// const sql = postgres(pgurl);
 
 export const auth = lucia({
-  adapter: postgresAdapter(sql, {
+  adapter: postgresAdapter({}, {
     user: "auth_user",
     key: "user_key",
     session: "user_session",
