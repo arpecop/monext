369ced5b8a9d4ac36f26c0385f1dd5095e4314aa
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-27 08:12:07 +0300

    xxx

--- apps/kloun/src/pages/api/img/[id].jpg.ts
+++ apps/kloun/src/pages/api/img/[id].jpg.ts
@@ -1,16 +1,15 @@
 import type { APIRoute } from "astro";
 
-import { gql } from "../../../components/gql";
+import { sql } from "../../../components/gql";
 export const prerender = false;
 
 export const get: APIRoute = async function get({ params }) {
   const id = params.id || "";
 
-  const datax = (await gql(`newssingle`, {
-    _eq: id,
-  })) as { newsbg_by_pk: { image: string } };
-
-  const response = await fetch(datax.newsbg_by_pk.image);
+  const datax = (await sql`select image from newsbg where nid = ${id}`) as {
+    image: string;
+  }[];
+  const response = await fetch(datax[0].image);
   const buffer = await response.arrayBuffer();
   return new Response(buffer, {
     status: 200,
