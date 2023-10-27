21af35423e6eebb18eb23b842d22e666a6bea2c9
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-27 11:07:46 +0300

    дсадасд

--- apps/kloun/astro.config.mjs
+++ apps/kloun/astro.config.mjs
@@ -1,19 +1,19 @@
 import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import { defineConfig } from "astro/config";
-
+// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare(),
-  integrations: [tailwind()],
-  vite: {
-    build: {
-      minify: false,
-    },
-    define: {
-      "process.env.DB_URL2": JSON.stringify(process.env.DB_URL2),
-      "process.env.DB_URL1": JSON.stringify(process.env.DB_URL1),
-    },
+  server: { port: 3000, host: true },
+  define: {
+    "process.env.DB_URL2": JSON.stringify(process.env.DB_URL2),
+    "process.env.DB_URL1": JSON.stringify(process.env.DB_URL1),
   },
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
 });
