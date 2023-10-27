83094595ecd9613eff747b6a248c0199ea205c88
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-27 13:52:01 +0300

    dasdsad

--- apps/kloun/astro.config.mjs
+++ apps/kloun/astro.config.mjs
@@ -4,13 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare(),
   server: { port: 3000, host: true },
-  define: {
-    "process.env.DB_URL2": JSON.stringify(process.env.DB_URL2),
-    "process.env.DB_URL1": JSON.stringify(process.env.DB_URL1),
-  },
   integrations: [
     tailwind({
       config: { applyBaseStyles: false },
