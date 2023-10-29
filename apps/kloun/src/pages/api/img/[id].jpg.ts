5815e2bd0a314e244b7d3522234b91d95f602b9e
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-29 12:49:49 +0200

    xxx

--- apps/kloun/src/pages/api/img/[id].jpg.ts
+++ apps/kloun/src/pages/api/img/[id].jpg.ts
@@ -1,21 +1,29 @@
 import type { APIRoute } from "astro";
 
-import { gql } from "../../../components/gql";
+import { sql } from "../../../../data/hasura";
 export const prerender = false;
 
 export const get: APIRoute = async function get({ params }) {
   const id = params.id || "";
 
-  const datax = (await gql(`newssingle`, {
-    _eq: id,
-  })) as { newsbg_by_pk: { image: string } };
-
-  const response = await fetch(datax.newsbg_by_pk.image);
-  const buffer = await response.arrayBuffer();
-  return new Response(buffer, {
-    status: 200,
-    headers: {
-      "Content-Type": "image/jpeg",
-    },
-  });
+  try {
+    const datax = (await sql`select image from newsbg where nid = ${id}`) as {
+      image: string;
+    }[];
+    const response = await fetch(datax[0].image);
+    const buffer = await response.arrayBuffer();
+    return new Response(buffer, {
+      status: 200,
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+    });
+  } catch (e) {
+    return new Response("", {
+      status: 200,
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+    });
+  }
 };
