19a9175698cc8093041636ca3e65d9d387222938
Radoslav Georgiev <rudix.lab@gmail.com>
2023-10-31 17:52:15 +0200

    xxx

--- apps/kloun/src/pages/api/img/[id].jpg.ts
+++ apps/kloun/src/pages/api/img/[id].jpg.ts
@@ -5,11 +5,8 @@ export const prerender = false;
 
 export const get: APIRoute = async function get({ params }) {
   const id = params.id || "";
-
   try {
-    const datax = (await sql`select image from newsbg where nid = ${id}`) as {
-      image: string;
-    }[];
+    const datax = await sql(`select image from newsbg where nid = '${id}'`);
     const response = await fetch(datax[0].image);
     const buffer = await response.arrayBuffer();
     return new Response(buffer, {
