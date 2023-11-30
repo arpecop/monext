60aa090d5a79b15ea2cef4623361047ee6740ca9
Radoslav Georgiev <rudix.lab@gmail.com>
2023-11-30 18:39:08 +0200

    xxx

--- apps/novoselska/astro.config.mjs
+++ apps/novoselska/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dr-novoselska.com/',
+  output: 'hybrid',
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
