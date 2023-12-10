5c6914593309b85f3696628591ddbb6c1676bc78
Radoslav Georgiev <rudix.lab@gmail.com>
2023-12-10 15:16:22 +0200

    xxx

--- apps/userz/src/pages/api/img/[id].ts
+++ apps/userz/src/pages/api/img/[id].ts
@@ -1,11 +1,11 @@
 
 // This is the function that returns the SVG code
-import { initWasm } from '@resvg/resvg-wasm';
+
 
 import type { APIRoute } from "astro";
 import satori from 'satori';
 import { html } from "satori-html";
-import resvgwasm from '../../../index_bg.wasm';
+
 
 
 
@@ -18,11 +18,7 @@ const robotoArrayBuffer = await (
 
 export const GET: APIRoute = async ({ params }) => {
 
-  try {
-    await initWasm(resvgwasm as WebAssembly.Module);
-  } catch (error) {
-    console.error('Resvg wasm not initialized');
-  }
+
   const markup = html`<div style="color: white; background-color: black; height: 400">hello, world1 ${params.id}</div>` as any;
 
   const svg = await satori(markup,
