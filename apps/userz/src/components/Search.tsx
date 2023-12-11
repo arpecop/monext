db78b0aa1d30b4e222e0b77c47e809b955a48228
Radoslav Georgiev <rudix.lab@gmail.com>
2023-12-11 18:40:50 +0200

    dasdasd

---
+++ apps/userz/src/components/Search.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+
+const Search: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+
+  };
+  // handle submit
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSearchTerm('');
+    alert(`Submitting Name ${searchTerm}`)
+  }
+
+
+  return (
+
+
+    <div
+      id="marketing-banner"
+      tabIndex={-1}
+      className="backdrop-blur-md bg-black/30 sticky z-50 flex flex-col md:flex-row justify-between py-4 top-0"
+    >
+      <div className="flex mb-3 me-4 flex-row grow items-center">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center mb- md:pe-4 md:me-4 md:border-e md:mb-0 border-black"
+        >
+          <img src="/ailogo.png" className="h-20 me-2" alt="Flowbite Logo" />
+        </a>
+
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="relative">
+            <div
+              className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+            >
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"></path>
+              </svg>
+            </div>
+            <input
+              value={searchTerm} onChange={handleSearch}
+              type="search"
+              id="default-search"
+              className="block p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search Mockups, Logos..."
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >Search</button
+            >
+          </div>
+        </form>
+      </div>
+      <div className="flex items-center flex-shrink-0">
+        <button
+          data-dismiss-target="#marketing-banner"
+          type="button"
+          className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"></path>
+          </svg>
+          <span className="sr-only">Close banner</span>
+        </button>
+      </div>
+    </div>
+
+  );
+};
+
+export default Search;
