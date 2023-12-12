342a36511e5ae99449f5254f1067cbc41dba5ab2
Radoslav Georgiev <rudix.lab@gmail.com>
2023-12-12 19:10:02 +0200

    xxx

--- apps/userz/src/lib/lucia.ts
+++ apps/userz/src/lib/lucia.ts
@@ -1,9 +1,11 @@
+import { prisma } from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { lucia } from "lucia";
 
+const client = new PrismaClient().$extends(withAccelerate())
 
-import { prisma } from "@lucia-auth/adapter-prisma";
-import { PrismaClient } from "@prisma/client";
-const client = new PrismaClient();
+
 
 
 export const auth = lucia({
