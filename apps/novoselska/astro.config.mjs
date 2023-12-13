002287a1c2247adf5117c859f7f47932b662e4bf
Radoslav Georgiev <rudix.lab@gmail.com>
2023-12-14 00:33:39 +0200

    xxx

--- apps/novoselska/astro.config.mjs
+++ apps/novoselska/astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: cloudflare(),
   integrations: [tailwind()],
+  vite: {
+    build: {
+      minify: false
+    },
+    define: {
+      "process.env.DB_URL2": JSON.stringify(process.env.DB_URL2),
+      "process.env.GSECRET": JSON.stringify(process.env.GSECRET)
+    }
+  }
 });
---
+++ apps/novoselska/src/middleware.ts
@@ -0,0 +1,14 @@
+
+
+import type { MiddlewareHandler } from "astro";
+import { auth } from './lib/lucia';
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+
+
+
+  context.locals.auth = auth.handleRequest(context);
+
+  return next();
+};
+
