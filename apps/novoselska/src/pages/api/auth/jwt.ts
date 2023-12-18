8e0f0f7bcd30b00d254006722d81d071332f0755
Radoslav Georgiev <rudix.lab@gmail.com>
2023-12-18 12:29:08 +0200

    Update JWT secret key generation

--- apps/novoselska/src/pages/api/auth/jwt.ts
+++ apps/novoselska/src/pages/api/auth/jwt.ts
@@ -1,13 +1,19 @@
-
 import type { APIRoute } from 'astro';
-import { createSecretKey } from "crypto";
 import { SignJWT } from "jose";
-export const prerender = false
-
+export const prerender = false;
 
 export async function GET(req: APIRoute) {
-  const secret = "1347cc301b614b6b264527e494148261";
-  const key = createSecretKey(Buffer.from(secret, "utf8"));
+  const secret = "1347cc301b614b6b264527e494148268";
+  const encoder = new TextEncoder();
+  const data = encoder.encode(secret);
+
+  const key = await crypto.subtle.importKey(
+    "raw",
+    data,
+    { name: "HMAC", hash: "SHA-256" },
+    false,
+    ["sign"]
+  );
 
   const token = await new SignJWT({})
     .setProtectedHeader({ alg: "HS256" })
