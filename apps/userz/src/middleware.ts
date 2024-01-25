e03f29700ac4de4403e01751c89edd7d2a777724
Radoslav Georgiev <rudix.lab@gmail.com>
2024-01-25 03:05:07 +0200

    Refactor user agent check in middleware.ts

--- apps/userz/src/middleware.ts
+++ apps/userz/src/middleware.ts
@@ -4,6 +4,9 @@ import type { MiddlewareHandler } from "astro";
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
   const userAgent = context.request.headers.get("user-agent");
+  const botDiff = context.request.cf.verifiedBotCategory
+
+
 
   // Check if the user agent is a popular bot like Google
   const isBot = userAgent && /googlebot|bingbot|yandexbot|slurp|duckduckbot/i.test(userAgent);
@@ -14,12 +17,9 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
   // Check if the user agent is a mobile browser
   const isMobileBrowser = userAgent && /android|iphone|ipad|ipod/i.test(userAgent);
 
-  if (isBot) {
-
-    console.log("Bot request detected");
-
-    return next();
-  } else if (isBrowser || isMobileBrowser) {
+  if (botDiff === "AI Crawler" || botDiff === "Search Engine Optimization") {
+    return;
+  } else if (isBrowser || isMobileBrowser || isBot) {
     return next();
   } else {
     return;
