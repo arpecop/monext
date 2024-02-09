47732781feef5a7ee6ead0896f80e9cc8d2ee1c9
Radoslav Georgiev <rudix.lab@gmail.com>
2024-02-09 23:45:11 +0200

    Update .env, hasura.ts, and package.json files

--- apps/kloun/data/hasura.ts
+++ apps/kloun/data/hasura.ts
@@ -1,43 +1,8 @@
-const urlh = "https://rudix.hasura.app/api/rest/";
+import { ApolloClient, InMemoryCache, gql as gqla } from '@apollo/client';
+export const gql = gqla;
+const client = new ApolloClient({
+  uri: 'https://hasura.kloun.lol/v1/graphql',
+  cache: new InMemoryCache(),
+});
 
-export async function sql(query: string) {
-  // Default options are marked with *
-  const response = await fetch("https://sql.monastro.workers.dev", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify({ query }), // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-export const viewh = (endpoint: string, params?: any) => {
-  const urlparams = new URLSearchParams(params).toString();
-  const res = fetch(`${urlh}${endpoint}${params ? "?" + urlparams : ""}`).then(
-    (response) => response.json()
-  );
-  return res;
-};
-
-export const geth = (id: string) => {
-  const res = fetch(`${urlh}${id}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "appication/json",
-      "x-hasura-role": "public",
-    },
-  })
-    .then((response) => response.json())
-    .catch((e) => e);
-  return res;
-};
-
-export default {
-  viewh,
-  geth,
-};
-
-//dasdasd
+export default client;
