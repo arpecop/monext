e5bc838714ebdb793630bb4aedac7c0582c10a0d
Radoslav Georgiev <rudix.lab@gmail.com>
2024-02-10 10:52:21 +0200

    Refactor API calls and update queries in kloun and tw pages

--- apps/kloun/src/pages/api/img/[id].jpg.ts
+++ apps/kloun/src/pages/api/img/[id].jpg.ts
@@ -1,12 +1,25 @@
 import type { APIRoute } from "astro";
 
-import { sql } from "../../../../data/hasura";
+import client, { gql } from "../../../../data/hasura";
 export const prerender = false;
 
 export const get: APIRoute = async function get({ params }) {
   const id = params.id || "";
   try {
-    const datax = await sql(`select image from newsbg where nid = '${id}'`);
+
+
+    const datax = await client.query({
+      query: gql`
+       query MyQuery($offset: Int = 0, $_eq: String = "") {
+      newsbg(limit: 30, offset: $offset, where: { nid: { _eq: $_eq } }) {
+        image
+      }
+    }
+  `,
+      variables: {
+        _eq: id,
+      },
+    });
     const response = await fetch(datax[0].image);
     const buffer = await response.arrayBuffer();
     return new Response(buffer, {
