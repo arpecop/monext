575514d5f68552009a4b1ca7f089eec0c8c34b38
Radoslav Georgiev <rudix.lab@gmail.com>
2024-02-25 12:43:54 +0200

    xxx

--- apps/kloun/data/hasura.ts
+++ apps/kloun/data/hasura.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, gql as gqla } from "@apollo/client";
 export const gql = gqla;
 const client = new ApolloClient({
-  uri: "http://130.204.65.82:8080/v1/graphql",
+  uri: "https://hasura.kloun.lol/v1/graphql",
   cache: new InMemoryCache(),
 });
 
