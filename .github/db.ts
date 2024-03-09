50e9f590c68c84254dace68606fb28ad2da85c68
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-09 13:42:19 +0200

    xxx

--- .github/db.ts
+++ .github/db.ts
@@ -9,7 +9,7 @@ export const gql = async (
   }
 ) => {
   return new Promise((resolve) => {
-    fetch("https://rudix.hasura.app/v1/graphql", {
+    fetch("https://hasura.kloun.lol/v1/graphql", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
