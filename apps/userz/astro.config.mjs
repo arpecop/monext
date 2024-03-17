230238c2c05932f8211bd565f9c9456b32c1a927
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-17 13:32:35 +0200

    xxx

--- apps/userz/astro.config.mjs
+++ apps/userz/astro.config.mjs
@@ -1,22 +1,25 @@
 import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
+import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react(), db()],
+  server: { port: 3000, host: true },
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    db(),
+  ],
   vite: {
     build: {
       minify: false,
     },
     define: {
       "process.env.DB_URL2": JSON.stringify(process.env.DB_URL2),
+      "process.env.DB_URL1": JSON.stringify(process.env.DB_URL1),
     },
   },
 });
