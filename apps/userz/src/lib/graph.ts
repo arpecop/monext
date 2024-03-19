aea99102ab5dac245d3feb30759d7076a61439a6
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-19 21:09:19 +0200

    xxx

--- apps/userz/src/lib/graph.ts
+++ apps/userz/src/lib/graph.ts
@@ -23,7 +23,7 @@ const questions = pgTable("questions", {
 export const db = drizzle(
   async (sql, params, method) => {
     try {
-      const rows = await fetch("http://130.204.65.82:3003", {
+      const rows = await fetch("https://sql.kloun.lol/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
