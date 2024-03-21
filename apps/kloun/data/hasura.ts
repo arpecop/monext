59bfec135d5768c9361612abe2c999c6e47fe1a5
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-21 16:58:59 +0200

    xxx

--- apps/kloun/data/hasura.ts
+++ apps/kloun/data/hasura.ts
@@ -1,3 +1,95 @@
+import { drizzle } from "drizzle-orm/pg-proxy";
+
+import { Lucia } from "lucia";
+import {
+  text,
+  integer,
+  pgSchema,
+  pgTable,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+
+import { eq, lt, gte, ne, and, or, sql } from "drizzle-orm";
+export { eq, lt, gte, ne, and, or, sql };
+
+const mySchema = pgSchema("q");
+const mySchema1 = pgSchema("u");
+export const q_q = mySchema.table("q", {
+  genid: text("genid").primaryKey(),
+  text: text("text").notNull(),
+  image: text("image"),
+  rand: integer("rand"),
+});
+export const q_qtags = mySchema.table("qtags", {
+  hashtag: text("hashtag").primaryKey(),
+  count: integer("count").notNull(),
+  rows_with_tag: text("rows_with_tag").notNull(),
+});
+export const q_a = mySchema.table("a", {
+  genid: text("genid").primaryKey(),
+  text: text("text").notNull(),
+});
+export const questions = pgTable("questions", {
+  genid: text("genid").primaryKey(),
+  text: text("text").notNull(),
+  image: text("image"),
+  type: text("type").notNull(),
+});
+export const db = drizzle(
+  async (sql, params, method) => {
+    try {
+      const rows = await fetch("http://sql.kloun.lol", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+
+        body: JSON.stringify({
+          sql,
+          params,
+          method,
+        }),
+      });
+      const json = await rows.json();
+      return { rows: json };
+    } catch (e: any) {
+      console.error("Error from pg proxy server: ", e.response);
+      return { rows: [] };
+    }
+  },
+  { schema: { q_a, q_q, questions } }
+);
+
+const userTable = mySchema1.table("user", {
+  id: text("id").primaryKey(),
+});
+
+const sessionTable = mySchema1.table("session", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: import.meta.env.PROD,
+    },
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+  }
+}
+
 export const gql = async (
   query: string,
   variables?: { [key: string]: unknown }
