22ad8de4a0d58e543bb9218be6edaf3a30ee4750
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-22 17:51:12 +0200

    xxx

--- apps/kloun/data/hasura.ts
+++ apps/kloun/data/hasura.ts
@@ -3,8 +3,8 @@ import { drizzle } from "drizzle-orm/pg-proxy";
 // import { Lucia } from "lucia";
 import { text, integer, pgTable, json } from "drizzle-orm/pg-core";
 
-import { eq, lt, gte, ne, and, or, sql, desc } from "drizzle-orm";
-export { eq, lt, gte, ne, and, or, sql, desc };
+import { eq, lt, gte, ne, le, and, or, sql, desc } from "drizzle-orm";
+export { eq, lt, gte, ne, le, and, or, sql, desc };
 
 export const questions = pgTable("questions", {
   genid: text("genid").primaryKey(),
@@ -26,6 +26,14 @@ export const newsbg = pgTable("newsbg", {
 
 export const agregator = pgTable("aggregator", {
   cat: text("cat_with_suffix").notNull().primaryKey(),
+  count: integer("total_count").notNull(),
+});
+
+export const jokes = pgTable("jokes", {
+  uid: text("uid").notNull().primaryKey(),
+  id: text("id").notNull(),
+  cat: text("cat").notNull(),
+  joke: text("joke").notNull(),
   count: integer("total_count").notNull(),
 });
 export const db = drizzle(
