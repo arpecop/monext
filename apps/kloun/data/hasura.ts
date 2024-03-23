58635d376d981bb40142c862cd5c33b1270cc6a3
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-23 14:33:55 +0200

    ььь

--- apps/kloun/data/hasura.ts
+++ apps/kloun/data/hasura.ts
@@ -72,22 +72,3 @@ export const db = drizzle(
 //     },
 //   },
 // });
-
-export const gql = async (
-  query: string,
-  variables?: { [key: string]: unknown }
-) => {
-  const response = await fetch("https://hasura.kloun.lol/v1/graphql", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  });
-
-  const json = await response.json();
-  if (json.errors) throw new Error(JSON.stringify(json.errors));
-
-  return json.data;
-};
