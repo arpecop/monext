819f5a8a9731f52322b321ff2632a143dbe4129f
Radoslav Georgiev <rudix.lab@gmail.com>
2024-03-25 12:13:45 +0200

    xxxx

--- apps/userz/src/lib/graph.ts
+++ apps/userz/src/lib/graph.ts
@@ -89,22 +89,22 @@ declare module "lucia" {
     Lucia: typeof lucia;
   }
 }
-
-export const gquery = async (
-  query: string,
-  variables?: { [key: string]: unknown }
-) => {
-  const response = await fetch("https://hasura.kloun.lol/v1/graphql", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  });
-
-  const json = await response.json();
-  if (json.errors) throw new Error(JSON.stringify(json.errors));
-
-  return Promise.resolve(json.data);
-};
+//
+// export const gquery = async (
+//   query: string,
+//   variables?: { [key: string]: unknown }
+// ) => {
+//   const response = await fetch("https://hasura.kloun.lol/v1/graphql", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       query,
+//       variables,
+//     }),
+//   });
+//
+//   const json = await response.json();
+//   if (json.errors) throw new Error(JSON.stringify(json.errors));
+//
+//   return Promise.resolve(json.data);
+// };
